eb49ca9329f86e99b937c88ba0b79b54a9224c4d
Evan Hellersund <ehhellersund@wm.edu>
2023-02-19 22:36:40 -0500

    Base code compiles & runs, initial commit. Baseline

---
+++ MazeByEvanHellersund/src/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author ehell
+ *
+ */
+module MazeByEvanHellersund {
+    requires transitive java.desktop;
+    requires junit;
+    requires org.junit.jupiter.api;
+    requires java.logging;
+    exports generation;
+    exports gui;
+
+}
